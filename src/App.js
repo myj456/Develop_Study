5ba38a76754f220224c2ca7be8a08850a36e3b42
myj456 <bkspringmyj@gmail.com>
2026-01-09 00:20:31 +0900

    리액트 강좌 #1~3 수강

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Tom";
+  // 객체 사용
+  const naver = {
+    name: "네이버",
+    url: "https://www.naver.com",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1
+        style={{
+          color: "#f0f",
+          backgroundColor: "green",
+        }}
+      >
+        Hello, {name}. <p>{2 + 3}</p>
+        <a href={naver.url}>{naver.name}</a>
+      </h1>
     </div>
   );
 }
